3dea0bd74527c3de7184c16d8a6e5af7e9a73328
rjimaw7 <rjimaw7777@gmail.com>
2024-02-09 12:25:43 +0800

    test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>MY BOILER PLATE FOR NEXT JS</h1>
+      <p>Test check if husky is working</p>
     </main>
   );
 }
